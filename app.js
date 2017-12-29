3715de52a45698b02990f4679cbf9770f0fa3dfe
Richard Bak <richard.bak@ithint.local>
2017-12-29 18:16:51 +0100

    streaming html

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var fs= require('fs');
 http.createServer(function(req, res){
     
     res.writeHead(200, { 'Content-Type': 'text/html'  });
-    var html = fs.readFileSync(__dirname+ '/index.htm', 'utf-8');
-    res.end(html);
-    
+    fs.createReadStream(__dirname+ '/index.htm').pipe(res);
+     
 
 }).listen(1337,'127.0.0.1');
